fed223f8b96e8ea7be4e1c3b713d446187e1d23b
MadNake <maksim.usalko@mail.ru>
2022-02-01 21:52:39 +0300

    Update flip-script.js

--- Open Lection 2021/flip-script.js
+++ Open Lection 2021/flip-script.js
@@ -1,8 +1,12 @@
-var flip = document.querySelector(".animation");
+var flip = document.querySelectorAll(".animation");
 
 console.log(flip);
+for (let i = 0;i< flip.length; i++) {
+  console.log(flip[i]);
 
-flip.addEventListener("click", function(evt) {
+  flip[i].addEventListener("click", function(evt) {
   evt.preventDefault();
-  flip.classList.add("animation-watch");
+  flip[i].classList.add("animation-watch");
+  setTimeout(function() {flip[i].classList.remove("animation-watch")}, 1000);
 });
+};
